52dc832cb9f87d65fc07100eaf3252ee65a79a79
Ding He <derrick.he@gmail.com>
2023-12-01 16:23:02 +0100

    First commit

---
+++ 2023/day1/solution_code.py
@@ -0,0 +1,39 @@
+# import library
+import re
+
+# create an empty list to store all the digits
+all_line_twoDigit = []
+
+# open the input file
+input = open("input.txt", 'r')
+#input = open("test_input.txt", 'r')
+
+#############################################################################################
+#             Part 1                                                                        #
+#############################################################################################
+
+# read through the lines, find all digits, and save the first one and the last combined
+#for line in input.readlines():
+#    all_line_twoDigit.append(re.findall(r'\d', line)[0]+re.findall(r'\d', line)[-1])
+
+#############################################################################################
+#             Part 2                                                                        #
+#############################################################################################
+lookup_dict_f = {"zero": "0", "0": "0", "one": "1", "1": "1", "two": "2", "2": "2", "three": "3", "3": "3", 
+               "four": "4", "4": "4", "five": "5", "5": "5", "six": "6", "6": "6", "seven": "7", "7": "7", 
+               "eight": "8", "8": "8", "nine": "9", "9": "9",}
+lookup_dict_r = {"zero"[::-1]: "0", "0": "0", "one"[::-1]: "1", "1": "1", "two"[::-1]: "2", "2": "2", "three"[::-1]: "3", "3": "3", 
+               "four"[::-1]: "4", "4": "4", "five"[::-1]: "5", "5": "5", "six"[::-1]: "6", "6": "6", "seven"[::-1]: "7", "7": "7", 
+               "eight"[::-1]: "8", "8": "8", "nine"[::-1]: "9", "9": "9",}
+
+pattern_f = '|'.join(k for k in lookup_dict_f)
+pattern_r = '|'.join(k for k in lookup_dict_r)
+
+for line in input.readlines():
+    #converted_line = re.sub(pattern, lambda m: lookup_dict.get(m.group(0)), line, flags=re.IGNORECASE)
+    first_digit = lookup_dict_f[re.findall(pattern_f, line)[0]]
+    last_digit = lookup_dict_r[re.findall(pattern_r, line[::-1])[0]]
+    all_line_twoDigit.append(first_digit + last_digit)
+
+# print the sum of the combined two digits
+print(sum(map(int, all_line_twoDigit)))
